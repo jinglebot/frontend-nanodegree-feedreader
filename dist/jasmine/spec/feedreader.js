80f657c76f76d8746e4337be7f0093caa2a12ec8
jinglebot <jinglebot@users.noreply.github.com>
2016-09-26 16:58:56 -0700

    Remove done in non-asynchronous functions

--- dist/jasmine/spec/feedreader.js
+++ dist/jasmine/spec/feedreader.js
@@ -43,10 +43,8 @@ $(function() {
 
     /* This writes a new test suite named "The menu" */
     describe('The Menu', function() {
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
+        /* This is a test that ensures the menu element is
+         * hidden by default.
          */
          it('should be hidden by default', function() {
             expect($('body').hasClass('menu-hidden')).toBe(true);
@@ -84,7 +82,6 @@ $(function() {
         it('should have at least one entry', function (done) {
             expect($('.feed').children()).toBeDefined();
             expect($('.feed .entry').length).not.toBe(0);
-            done();
         });
     });
 
